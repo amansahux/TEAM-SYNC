77595669438237c7351255b4c84a982fc2fca547
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-09-06 22:40:33 +0530

    feat: add useEmployees hook for fetching paginated employee data via React Query

--- src/features/admin module/employees/hooks/useEmployee.jsx
+++ src/features/admin module/employees/hooks/useEmployee.jsx
@@ -1,11 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 import { getAllEmployees } from "../apis/employees.api";
 
-export const useEmployees = (page = 1) => {
+export const useEmployees = (page = 1,limit = 20) => {
 const {data, isPending, error} = useQuery({
 queryKey:["employees", page],
-queryFn: () => getAllEmployees(page),
+queryFn: () => getAllEmployees(page, limit),
 staleTime: 5 * 100000 , // 5 minutes
+keepPreviousData: true,
 });
   return { data, isPending, error };
 };

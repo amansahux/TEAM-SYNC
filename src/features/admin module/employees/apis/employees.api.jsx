2e6e8941e3af0af5708850bfc562eb0ff2817a8e
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-09-06 22:29:23 +0530

    feat: add getAllEmployees API function to fetch employee data with pagination

--- src/features/admin module/employees/apis/employees.api.jsx
+++ src/features/admin module/employees/apis/employees.api.jsx
@@ -1,5 +1,7 @@
-import axiosInstance  from '../../../../app/config/axiosInstance';
-export const getAllEmployees = async (page = 1)=>{
-    const response = await axiosInstance.get(`/employee?limit=20&page=${page}`);
-    return response.data.data;
-}
+import axiosInstance from "../../../../app/config/axiosInstance";
+export const getAllEmployees = async (page = 1, limit = 20) => {
+  const response = await axiosInstance.get(
+    `/employee?limit=${limit}&page=${page}`,
+  );
+  return response.data.data;
+};

d3a4d5b8b876e46a9d748a7401980332e746e645
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-09-11 13:59:15 +0530

    feat: implement real-time chat functionality using Socket.io for frontend messaging and server-side relay

--- Backend/server.js
+++ Backend/server.js
@@ -18,6 +18,11 @@ const io = new Server(httpServer, {
 
 io.on("connection", (socket) => {
     console.log("User connected:", socket.id);
+    socket.on("message:send", (data) => {
+        console.log("Message received:", data);
+
+        io.emit("message:new", data);
+    });
 
     socket.on("disconnect", () => {
         console.log("User disconnected:", socket.id);
--- Frontend/src/features/chats/ui/page/Chat.jsx
+++ Frontend/src/features/chats/ui/page/Chat.jsx
@@ -1,18 +1,65 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import socket from "../../socket/socket";
 
 const Chat = () => {
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+
   useEffect(() => {
-    const res = socket.connect();
-    if (res.connected) {
-      console.log("Connected to socket", res.id);
-    }
+    socket.connect();
+
+    const handleNewMessage = (newMessage) => {
+      setMessages((prev) => [
+        ...prev,
+        newMessage,
+      ]);
+    };
+
+    socket.on("message:new", handleNewMessage);
 
     return () => {
+      socket.off("message:new", handleNewMessage);
       socket.disconnect();
     };
-  }, [socket]);
-  return <div>Chat Page</div>;
+  }, []);
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+
+    if (!message.trim()) return;
+
+    socket.emit("message:send", {
+      content: message.trim(),
+    });
+
+    setMessage("");
+  };
+
+  return (
+    <div>
+      <h1>Team Chat</h1>
+
+      <div>
+        {messages.map((message, index) => (
+          <div key={index}>
+            {message.content}
+          </div>
+        ))}
+      </div>
+
+      <form onSubmit={handleSendMessage}>
+        <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Type a message..."
+        />
+
+        <button type="submit">
+          Send
+        </button>
+      </form>
+    </div>
+  );
 };
 
 export default Chat;

a9746ac2a2f42aa7843f065086cdcf86dedffb8d
Aman Kumar Sahu <amansahu1126@gmail.com>
2026-09-25 18:18:51 +0530

    setup of task for employee side

--- Frontend/src/app/routes/EmployeeRoutes.jsx
+++ Frontend/src/app/routes/EmployeeRoutes.jsx
@@ -1,9 +1,14 @@
 
 import MyTask from "../../features/employee module/MyTask/ui/page/MyTask";
+import TaskDetail from "../../features/employee module/MyTask/ui/page/TaskDetail";
 
 export const employeeRoutes = [
     {
         path:"my-task",
         element:<MyTask/>
+    },
+    {
+        path:"my-task/:id",
+        element:<TaskDetail/>
     },
 ]
